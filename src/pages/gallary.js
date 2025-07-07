cd7308bcf45fe2f497df41e00ffa28598c336a48
CrisL305 <cristopher@allapattahcdc.org>
2025-07-07 12:53:30 -0400

    this commit contains the pages that will be used with the correct export. App.js is importing all the pages and redering the correct text on the DOM. The navivation created using routes are working. html can be added now.

---
+++ src/pages/gallary.js
@@ -0,0 +1,7 @@
+const Gallary = () => {
+    return (
+        <h1>This is the gallary page</h1>
+    )
+}
+
+export default Gallary;
---
+++ src/pages/services.js
@@ -0,0 +1,7 @@
+const Services = () => {
+    return (
+        <h1>This is the services page</h1>
+    )
+}
+
+export default Services;
