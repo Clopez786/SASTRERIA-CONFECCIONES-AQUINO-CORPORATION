cedd55ba0d449deaea0a50fa0abe3b790ce60cef
CrisL305 <cristopher@allapattahcdc.org>
2025-07-07 17:36:08 -0400

    This commit contains the global style that has the color theme of the website as well as variables. The home page html informaiton is included as well as the header and footer. Next step is adding css to header, footer, and home page.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,34 @@
+import heroImage from "../assets/images/HeroImg.png";
+import Button from "../components/Btn/button.js";
+import Scissors from "../assets/images/scissors.png";
+import Thread from "../assets/images/thread_spool.png";
+import Shirt from "../assets/images/shirt.png";
+
 const Home = () => {
     return (
-        <h1>This is the home page</h1>
+        <div>
+            <div className="hero">
+                <img className="hero__image" src={heroImage} alt="" />
+                <div>
+                    <p>Crafted Elegance. <br /> Tailord to You.</p>
+                    <Button txt={"contact us"} />
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src={Scissors} alt="" />
+                    <h2>Custom Tailoring</h2>
+                </div>
+                <div>
+                    <img src={Thread} alt="" />
+                    <h2>Alterations & Repairs</h2>
+                </div>
+                <div>
+                    <img src={Shirt} alt="" />
+                    <h2>Ready to Wear Collections</h2>
+                </div>
+            </div>
+        </div>
     )
 }
 
